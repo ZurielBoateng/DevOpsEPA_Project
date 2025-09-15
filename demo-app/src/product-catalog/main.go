1b09d8cc152a89a371ac13ca4952bd811badc2e6
Z <105754053+ZurielBoateng@users.noreply.github.com>
2025-09-15 12:29:01 +0100

    Update main.go

--- demo-app/src/product-catalog/main.go
+++ demo-app/src/product-catalog/main.go
@@ -1,6 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 // Dummy change cicd test 1
+// new change
 package main
 
 //go:generate go install google.golang.org/protobuf/cmd/protoc-gen-go
